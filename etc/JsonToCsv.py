b6e6c473cee8b952b3735682083e04c3e70bfdc7
yunaisme <57870185+yunaisme@users.noreply.github.com>
2021-09-10 19:53:32 +0900

    Create JsonToCsv.py

---
+++ etc/JsonToCsv.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+
+ #This example uses Python 2.7 and the python-request library.
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'e5464800-984f-410e-9be0-9f553f5890f4',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+info = json.loads(response.text)
+df = pd.json_normalize(info['data'])
+df.to_csv("./sample.csv")
